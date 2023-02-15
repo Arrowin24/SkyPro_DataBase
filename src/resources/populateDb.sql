3ee74777fda5f7e9014bc055f33ef1fa7977795a
Arrowin24 <igor_rudenko.ru@mail.ru>
2023-02-15 21:31:49 +0700

    Solution

--- src/resources/populateDb.sql
+++ src/resources/populateDb.sql
@@ -92,3 +92,82 @@ WHERE age = (SELECT MAX(Возраст)
                    FROM employee
                    GROUP BY first_name
                    HAVING count(first_name) > 1) as eM);
+
+/*Задание 1.1*/
+CREATE TABLE IF NOT EXISTS city
+(
+    city_id   SERIAL PRIMARY KEY,
+    city_name VARCHAR(50) NOT NULL
+);
+
+
+/*Задание 1.2*/
+ALTER TABLE employee
+    ADD COLUMN city_id varchar(50);
+
+ALTER TABLE employee
+    ALTER COLUMN city_id TYPE INTEGER USING city_id::integer;
+
+/*Задание 1.3*/
+ALTER TABLE employee
+    ADD FOREIGN KEY (city_id) REFERENCES city (city_id);
+
+/*Задание 1.4*/
+INSERT INTO city
+VALUES (1, 'Moscow'),
+       (2, 'Novosibirsk'),
+       (3, 'Pavlodar');
+
+UPDATE employee
+SET city_id=1
+WHERE id = 1;
+UPDATE employee
+SET city_id=2
+WHERE id = 2;
+UPDATE employee
+SET city_id=3
+WHERE id = 3;
+UPDATE employee
+SET city_id=1
+WHERE id = 4;
+UPDATE employee
+SET city_id=2
+WHERE id = 5;
+
+SELECT *
+FROM employee;
+
+/*Задание 2.1*/
+SELECT first_name AS Имя,
+       last_name  AS Фамилия,
+       city_name
+
+FROM employee
+         INNER JOIN city ON employee.city_id = city.city_id
+ORDER BY Имя;
+
+INSERT INTO city
+VALUES (4, 'NullCity');
+
+/*Задание 2.2*/
+SELECT city_name  AS Город,
+       first_name AS Имя,
+       last_name  AS Фамилия
+FROM employee
+         RIGHT JOIN city ON employee.city_id = city.city_id
+ORDER BY Город;
+
+/*Задание 2.3*/
+SELECT city_name  AS Город,
+       first_name AS Имя,
+       last_name  AS Фамилия
+FROM employee
+         FULL OUTER JOIN city ON employee.city_id = city.city_id
+ORDER BY Город;
+
+/*Задание 2.4*/
+SELECT first_name AS Имя,
+       city_name  AS Город
+FROM employee
+         LEFT JOIN city ON employee.city_id = city.city_id
+ORDER BY Город;
